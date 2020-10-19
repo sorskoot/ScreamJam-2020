dfc9df53e7964de10f6f351ca2e4e54325bf5d0c
Sorskoot <mental_c@hotmail.com>
2020-10-20 00:27:34 +0200

    Working on the jumpscare

---
+++ TheGarage/Assets/_Game/Scripts/TurnOffDarkness.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Projector))]
+public class TurnOffDarkness : MonoBehaviour
+{
+    private Projector projector;
+
+    private void Start() {
+        this.projector = GetComponent<Projector>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "FOVCollider"){
+            projector.enabled = false;
+        }
+    }
+}
---
+++ TheGarage/credits.md
@@ -0,0 +1,7 @@
+-- Game Credits --
+
+The games is made possible with the subscriptions of:
+
+- The_GhostRS
+- Davidwesst
+
